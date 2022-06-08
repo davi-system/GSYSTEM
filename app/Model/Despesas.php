22f69632d53db48cf84f4d0bd5a2bf1289c0d3ac
Davi <davialexandre.contato@hotmail.com>
2022-06-07 22:47:15 -0300

    add modal despesas

---
+++ app/Model/Despesas.php
@@ -0,0 +1,40 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+class Despesas extends AppModel {
+
+    var $useTable = 'despesas';
+    public $primaryKey = 'des_id';
+    public $displayField = 'des_descricao';
+    public $alias = 'des';
+
+    function listaDespesas($tipo, $descricao)
+    {        
+        if($tipo == 0) {
+            $where = "upper(des_descricao) like '%$descricao%'";
+        } else {
+            $where = "upper(tip_descricao) like '%$descricao%'";
+        }
+
+        return $this->query(
+            "SELECT 
+                des.des_id, 
+                des.des_tipo_fk, 
+                tip.tip_descricao, 
+                des.des_frp_fk, 
+                frp.frp_descricao, 
+                des.des_descricao, 
+                des.des_valor, 
+                des.des_parcela, 
+                des.des_dtcriacao, 
+                des.des_horacriacao
+            FROM 
+                despesas AS des
+            INNER JOIN tipos AS tip ON tip.tip_id = des.des_tipo_fk
+            INNER JOIN forma_pagamento AS frp ON frp.frp_id = des.des_frp_fk
+            WHERE $where
+            ORDER BY des_descricao"
+        );
+    }
+}
