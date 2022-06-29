7f64d50ec5bc76cf7680e18faf30ee095520cfa7
Davi <davialexandre.contato@hotmail.com>
2022-06-28 23:22:32 -0300

    add controller suporte

---
+++ app/Controller/SuporteController.php
@@ -0,0 +1,13 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+class SuporteController extends AppController {
+
+    var $uses = array('Suporte');
+
+    public function index()
+    {
+        # code...
+    }
+}
