8c4ae5d6c3a7d8866ad482e910458e26572f0f05
Davi <davialexandre.contato@hotmail.com>
2022-06-29 20:43:27 -0300

    add metodo

--- app/Model/Suporte.php
+++ app/Model/Suporte.php
@@ -8,4 +8,11 @@ class Suporte extends AppModel {
     public $primaryKey = 'sup_id';
     public $displayField = 'sup_descricao';
     public $alias = 'sup';
+
+    function inativarHistoricoChamados($codUsuario)
+    {
+        return $this->query(
+            "UPDATE suporte set sup_situacao = 'I' where sup_situacao = 'A' and sup_usu_fk = $codUsuario"
+        );
+    }
 }
