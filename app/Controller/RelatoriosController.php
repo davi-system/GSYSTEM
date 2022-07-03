65be8b197f3a4db4b6e8c4eebcca0ca5a05368cc
Davi <davialexandre.contato@hotmail.com>
2022-07-03 20:18:25 -0300

    chamando um componente

--- app/Controller/RelatoriosController.php
+++ app/Controller/RelatoriosController.php
@@ -4,6 +4,8 @@ App::uses('Controller', 'Controller');
 
 class RelatoriosController extends AppController {
 
+    public $components = array("Utilitarios");
+
     var $uses = array('Despesas');    
 
     public function index()
@@ -13,24 +15,18 @@ class RelatoriosController extends AppController {
         if($this->request->is('post')) {            
 
             $data1 = $this->request->data['Rel']['data1'];
-            $data2 = $this->request->data['Rel']['data2'];            
+            $data2 = $this->request->data['Rel']['data2'];                        
     
             $this->set('despesas', $this->Despesas->relatorioDespesas(
                 $usuario['usu']['usu_id'], 
-                $this->formataData($data1), 
-                $this->formataData($data2)
+                $this->Utilitarios->formataData($data1), 
+                $this->Utilitarios->formataData($data2)
             ));            
         }
         
     }
 
-    public function formataData($data)
-    {
-        $date = DateTime::createFromFormat('d/m/Y', $data);
-        return $date->format('Y-m-d');
-    }
-
-    public function exportarExcel()
+    public function r01Excel()
     {
         # code...
     }
