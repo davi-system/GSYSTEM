165360f938bcb5038b9f3784e608bdee26bd67dd
Davi <davialexandre.contato@hotmail.com>
2022-07-08 23:14:08 -0300

    add metodo do relatorio

--- app/Controller/RelatoriosController.php
+++ app/Controller/RelatoriosController.php
@@ -11,23 +11,32 @@ class RelatoriosController extends AppController {
     public function index()
     {
         $usuario = $this->Session->read('Person.usuario');
+        $this->set('usuario', $usuario['usu']['usu_id']);         
 
         if($this->request->is('post')) {            
 
             $data1 = $this->request->data['Rel']['data1'];
-            $data2 = $this->request->data['Rel']['data2'];                        
+            $data2 = $this->request->data['Rel']['data2'];
+            
+            $this->set('data1', $data1);
+            $this->set('data2', $data2);
     
             $this->set('despesas', $this->Despesas->relatorioDespesas(
                 $usuario['usu']['usu_id'], 
                 $this->Utilitarios->formataData($data1), 
                 $this->Utilitarios->formataData($data2)
             ));            
-        }
-        
+        }    
     }
 
-    public function r01Excel()
-    {
-        # code...
+    public function r01Excel($usuario, $data1, $data2)
+    {        
+        $this->layout = null;        
+
+        $this->set('despesas', $this->Despesas->relatorioDespesas(
+            $usuario,
+            $this->Utilitarios->formataData($this->Utilitarios->ajuntaFormataData($data1)), 
+            $this->Utilitarios->formataData($this->Utilitarios->ajuntaFormataData($data2))
+        ));        
     }
 }
